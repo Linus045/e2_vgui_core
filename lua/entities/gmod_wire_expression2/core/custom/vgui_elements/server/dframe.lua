ebe0fc1da5aa6bebab5c04fbdcbf295b4c6e8607
Vurv <56230599+Vurv78@users.noreply.github.com>
2022-09-05 23:54:43 -0700

    Parity with wiremod/wire#2399
    **DO NOT MERGE THIS UNTIL https://github.com/wiremod/wire/pull/2399 IS MERGED.**
    
    This PR converts functions that return a table with three numbers to proper Vector/Angle userdata, since that is what vector and angle types will now use in E2 for efficiency.
    
    I'm sweeping through found instances of this on github and making these PRs to make sure the amount of extensions broken by this is minimal.

--- lua/entities/gmod_wire_expression2/core/custom/vgui_elements/server/dframe.lua
+++ lua/entities/gmod_wire_expression2/core/custom/vgui_elements/server/dframe.lua
@@ -196,9 +196,9 @@ do--[[getter]]--
     e2function vector dframe:getColor(entity ply)
         local col =  E2VguiCore.GetPanelAttribute(ply,self.entity:EntIndex(),this,"color")
         if col == nil then
-            return {0,0,0}
+            return Vector(0, 0, 0)
         end
-        return {col.r,col.g,col.b}
+        return Vector(col.r,col.g,col.b)
     end
 
     e2function vector4 dframe:getColor4(entity ply)
